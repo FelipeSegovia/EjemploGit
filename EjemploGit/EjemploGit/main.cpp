f7a453900246e7a8ca0ef6a5ce2c494ab1d835be
Felipe <felipe.segovia.rod@gmail.com>
2018-02-17 20:14:25 -0300

    insertando codigo al main

--- EjemploGit/EjemploGit/main.cpp
+++ EjemploGit/EjemploGit/main.cpp
@@ -1,1 +1,14 @@
 #include "MiVentana.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+void Main()
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	EjemploGit::MiVentana form;
+	Application::Run(%form);
+}
